0e747c4d6a49fc01c2c16fa5fbf492a8d6c15d9b
Rails Udemy Course <railsudemycourse@gmail.com>
2016-05-03 19:33:29 +0000

    Remove max image size for testing

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,13 +1,13 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
-  validate :picture_size
+  #validate :picture_size
   
-  private
+  #private
   
-  def picture_size
-    if picture_size > 10.megabytes
-      errors.add(:picture, "should be less than 10MB")
-    end
-  end
+  #def picture_size
+    #if picture_size > 10.megabytes
+      #errors.add(:picture, "should be less than 10MB")
+    #end
+  #end
 end
